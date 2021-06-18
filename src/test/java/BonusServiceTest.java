a84e1fad3987e5426f6bab53d0676af1f6cadeda
Mikhail Ponomarev <michailp800@gmail.com>
2021-06-18 19:28:55 +0300

    BonusServiceTest fix

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -40,8 +40,8 @@ class BonusServiceTest {
 
     // подготавливаем данные:
     long amount = 1000_60;
-    boolean registered = true;
-    long expected = 30;
+    boolean registered = false;
+    long expected = 10;
 
     // вызываем целевой метод:
     long actual = service.calculate(amount, registered);
@@ -56,7 +56,7 @@ class BonusServiceTest {
 
     // подготавливаем данные:
     long amount = 1_000_000_60;
-    boolean registered = true;
+    boolean registered = false;
     long expected = 500;
 
     // вызываем целевой метод:
